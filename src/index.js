67e6a19afde0483ed694296f5ec1d5de35affddc
Izorrai <latxiondo.jon@gmail.com>
2024-11-12 16:08:57 +0100

    creacion de nueva rama para trabajar con las vistas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 
+app.set(`views`,'src/views');
+app.set(`view engine`,'pug');
+
+app.use(express.static('public'))
+
+app.get('/',(req, res) => {res.render('index', {tittle: 'Inicio', message: 'Mensaje a mostrar'})});
+
+
+app.listen(3000, () => console.log("Estamos conectados a la 3000"));
