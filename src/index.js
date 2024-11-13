066c216449505a9977efb917824f8b8658508c0d
Izorrai <latxiondo.jon@gmail.com>
2024-11-13 15:24:06 +0100

    login creado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+import express from "express";
 
-app.set(`views`,'src/views');
-app.set(`view engine`,'pug');
+
+const app = express();
 
 app.use(express.static('public'))
+app.set('views', 'src/views');
+app.set('view engine', 'pug');
 
-app.get('/',(req, res) => {res.render('index', {tittle: 'Inicio', message: 'Mensaje a mostrar'})});
+app.use(express.urlencoded({ extended: true }));// configurar body parser para recibir datos de formularios
+app.use(express.json());// configurar body parser para recibir datos en formato json
+
+
+app.get("/",(req, res) => {res.render("index", {tittle: 'Inicio', message: "Pagina Alquiler Bicicletas"})});
+
+app.get("/login", (req, res) => {
+    res.render("login", {
+      title: 'BilboBike'
+    });
+  });
+  
+  app.post('/login', (req, res) => {
+    const { usuario, contraseña } = req.body;
+  
+    if (usuario === "miusuario" && contraseña === "micontraseña") {
+      res.send("¡Inicio de sesión exitoso!");
+    } else {
+      res.render("login", {
+        title: "Inicio de Sesión",
+        error: "Usuario o contraseña incorrectos."
+      });
+    }
+  });
 
 
 app.listen(3000, () => console.log("Estamos conectados a la 3000"));
