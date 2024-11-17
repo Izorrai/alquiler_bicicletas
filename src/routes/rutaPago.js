0a7c66707eac64c8acd10640716513172339c46d
Izorrai <latxiondo.jon@gmail.com>
2024-11-17 12:29:44 +0100

    Crud De bicicletas y pagos actualizado y funciona correctamente, falta crear pagos

---
+++ src/routes/rutaPago.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import controladorMostrarPagos from "../controllers/pagos/controladorMostrarPago.js"
+const router = Router();
+
+router.get("/lista",controladorMostrarPagos.getAll);
+router.get("/nuevo",controladorMostrarPagos.crearFormulario);
+router.post("/nuevo",controladorMostrarPagos.crear)
+router.get("/:id",controladorMostrarPagos.mostrarPorId);
+router.get("/:id/actualizar",controladorMostrarPagos.actualizarFormPago)
+router.post("/:id/actualizar",controladorMostrarPagos.actualizarPago);
+router.post("/:id/eliminar",controladorMostrarPagos.eliminar)
+
+
+
+export default router;
