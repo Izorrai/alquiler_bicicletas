20be74748e1cfd649bf4c035e964e24476ed5fed
saulm96 <saulmorahernandez96@gmail.com>
2024-11-18 19:18:48 +0100

    Problemas de conflictos solucionados

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,23 +1,15 @@
 import { Router } from "express";
-import rutaBicicleta from "./rutaBicicleta.js"
-import rutaUsuario from "./rutaUsuario.js"
-import rutaPago from "./rutaPago.js"
-<<<<<<< HEAD
-import rutaUbicacion from "./rutaUbicacion.js"
-
-=======
+import rutaBicicleta from "./rutaBicicleta.js";
+import rutaUsuario from "./rutaUsuario.js";
+import rutaPago from "./rutaPago.js";
+import rutaUbicacion from "./rutaUbicacion.js";
 import rutaAlquileres from "./rutaAlquileres.js";
->>>>>>> dev
 const router = Router();
 
-router.use("/bicicletas",rutaBicicleta);
-router.use("/pagos",rutaPago);
-<<<<<<< HEAD
-router.use ("/ubicaciones", rutaUbicacion);
-=======
->>>>>>> dev
-router.use("/usuarios", rutaUsuario)
+router.use("/bicicletas", rutaBicicleta);
+router.use("/pagos", rutaPago);
+router.use("/ubicaciones", rutaUbicacion);
+router.use("/usuarios", rutaUsuario);
 router.use("/alquileres", rutaAlquileres);
-
 
 export default router;
